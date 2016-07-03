b70b7df468648b31ff96172af4c745615ddaaac8
Peter Schüllermann <peter.schuellermann@gmail.com>
2016-07-03 11:03:30 +0200

    Add map from here

--- src/js/map.js
+++ src/js/map.js
@@ -13,6 +13,27 @@ var adata;
 var dataa = [];
 
 
+// initialize map
+var platform = new H.service.Platform({
+  'app_id': 'vsQZSmhujvOERj1pbKY8',
+  'app_code': 'ZxL5RxiGcIISDFlQ7V3irQ'
+});
+
+
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('map'),
+  defaultLayers.normal.map,
+  {
+    zoom: 10,
+    center: { lat: 52.5, lng: 13.4 }
+  });
+
+
 relayr.login({
   success: function(token){
             console.log(token);
